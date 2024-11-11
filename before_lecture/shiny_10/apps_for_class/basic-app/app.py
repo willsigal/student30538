39505122e0176a31882aa74118f73fecc53b429e
Peter Ganong <ganong@gmail.com>
2024-11-11 08:51:50 -0600

    slides and apps for Monday class

---
+++ before_lecture/shiny_10/apps_for_class/basic-app/app.py
@@ -0,0 +1,19 @@
+from shiny import App, render, ui
+import matplotlib.pyplot as plt
+import numpy as np
+
+app_ui = ui.page_fluid(
+    ui.panel_title("Hello Shiny!"),
+    ui.input_slider("n", "N", 0, 100, 20),
+    ui.output_text_verbatim("txt"),
+)
+
+
+def server(input, output, session):
+    @render.text
+    def txt():
+        return f"n*2 is {input.n() * 2}"
+
+
+
+app = App(app_ui, server)
